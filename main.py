1aaabb3aeda8c07c22608c2c4336259e12c1b2c5
prathamngundikere <prathamgundikere@gmail.com>
2024-10-31 22:34:17 +0530

    Learned about GET and POST request

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randrange
 from typing import Optional
 
 from fastapi import FastAPI
@@ -11,16 +12,43 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [
+    {
+        "title": "title of post 1",
+        "content": "content of post 1",
+        "id": 1
+    },{
+        "title": "title of post 1",
+        "content": "content of post 2",
+        "id": 2
+    }
+]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")
 async def root():
     return {"test-message": "Hello World"}
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
 
-@app.post("/create-posts")
-async def create_posts(new_post: Post):
-    print(new_post)
-    return {"message": new_post.dict()}
+@app.post("/posts")
+async def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"message": post_dict}
+
+@app.get("/posts/{id}")
+async def get_post(id):
+    post = find_post(int(id))
+    return {
+        "message": "You selected post {id}",
+        "post-detail": post
+    }
